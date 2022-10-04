d2df7432e9d8626e212d21bbd14a65751cee175a
Ersin ÇETİN <ersin.cetin@hotmail.com>
2022-10-04 16:39:44 +0300

    Update

---
+++ src/main/java/com/restfullapi/RestfullApiApplication.java
@@ -0,0 +1,13 @@
+package com.restfullapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfullApiApplication.class, args);
+	}
+
+}
